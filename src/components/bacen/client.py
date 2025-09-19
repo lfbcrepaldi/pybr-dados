1ab75e249bcf0fbe54c9efe7d744c568ee6cca19
Luís Felipe <luis@bassofidc.com.br>
2025-09-19 10:35:50 -0300

    parametro ultimos criado taxa de cambio adicionada aos modelos

--- src/components/bacen/client.py
+++ src/components/bacen/client.py
@@ -7,7 +7,14 @@ class BacenClient:
     BASE_URL = 'https://api.bcb.gov.br/dados/serie/'
 
 
-    def sgs(self, codigo_serie: int, data_inicial: Optional[date] = None, data_final: Optional[date] = None, formato: Literal['json', 'csv'] = 'json') -> dict | str:
+    def sgs(
+        self, 
+        codigo_serie: int, 
+        data_inicial: Optional[date] = None, 
+        data_final: Optional[date] = None, 
+        ultimos: int | None = None, 
+        formato: Literal['json', 'csv'] = 'json'
+    ) -> dict | str:
         params = {
             'formato': formato,
         }
@@ -15,10 +22,13 @@ class BacenClient:
             params['dataInicial'] = data_inicial.strftime('%d/%m/%Y')
         if data_final:
             params['dataFinal'] = data_final.strftime('%d/%m/%Y')
-        url = f'{self.BASE_URL}bcdata.sgs.{codigo_serie}/dados'
+
+        suffix = f'/ultimos/{ultimos}' if ultimos else ''
+
+        url = f'{self.BASE_URL}bcdata.sgs.{codigo_serie}/dados{suffix}'
         response = requests.get(url, params=params)
         if not response.ok:
-            raise BacenAPIError(f'Erro ao acessar API Bacen: {response.status_code}')
+            raise BacenAPIError(f'Erro ao acessar API Bacen: {response.status_code}: {response.text}')
         if formato == 'csv':
             return response.text
         return response.json()
