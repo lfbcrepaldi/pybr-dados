762207e4107beef1a10cc2b10757bd65791e9f7b
Luís Felipe <luis@bassofidc.com.br>
2025-10-01 16:35:43 -0300

    Adiciona variáveis de instância para consulta de séries temporais e expectativas de mercado no BacenClient

--- src/components/bacen/__init__.py
+++ src/components/bacen/__init__.py
@@ -2,10 +2,20 @@ from .client import BacenClient
 from .models import SGSCodigoSerie, ExpectativasMercadoRelatorio, PTAXRecursos
 from .exceptions import BacenAPIError
 
+_client = BacenClient()
+
+consulta_series_temporais = _client.sgs
+expectativas_mercado = _client.expectativas
+emissao_moedas_anual = _client.emissao_moedas_anual
+ptax = _client.ptax
+
 __all__ = [
-    'BacenClient',
-    'SGSCodigoSerie',
-    'BacenAPIError',
-    'ExpectativasMercadoRelatorio',
-    'PTAXRecursos'
+    "consulta_series_temporais",
+    "expectativas_mercado",
+    "emissao_moedas_anual",
+    "ptax",
+    "SGSCodigoSerie",
+    "BacenAPIError",
+    "ExpectativasMercadoRelatorio",
+    "PTAXRecursos",
 ]
